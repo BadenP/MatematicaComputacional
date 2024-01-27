3938452c28d4f7ab4c1408b2824c207776a8674e
Luca <lucafidideus@gmail.com>
2024-01-27 17:01:16 -0300

    lut pronta

--- balley.py
+++ balley.py
@@ -1,20 +1,74 @@
-from math import ceil, log, exp
+from math import ceil, log, exp, floor
 from IEEE754 import Dec2IEEE
 import matplotlib.pyplot as plt
 
 ln2 = log(2)
 
+def fracao_da_mantissa(mantissa_binaria):
+    decimal = 0.0
+    expoente = -1
+
+    for bit in mantissa_binaria:
+        if bit == '1':
+            decimal += 2**expoente
+        expoente -= 1
+
+    return decimal
+
+
+def decimal_para_binario(numero_decimal):
+    if numero_decimal == 0:
+        return '0'
+    
+    binario = ''
+    while numero_decimal > 0:
+        resto = numero_decimal % 2
+        binario = str(resto) + binario
+        numero_decimal //= 2
+
+    return binario
+
+def mantissa_ieee(num: Dec2IEEE):
+    bin = decimal_para_binario(num.Fbits.f)
+    mantissa = 1 + fracao_da_mantissa(bin)
+    return mantissa
+
 # Objetivo: cálculo de e^x reduzindo o valor do argumento
 # e^x = 2^n * (e^r)^256
+
+'''
+colocar no relatorio que fazemos uma redução do valor do argumento pq o erro se propaga muito rapido 
+com numeros grandes se utilizarmos a serie de taylor. Entao reduzimos o valor do argumento para calcular a serie de taylow
+para um valor bem pequeno e calcula e^x de um valor inteiro q ja e facil de calcular ai no final so faz e^argumento = e^X * e^r
+'''
 
 def exponencial(x: Dec2IEEE):
     #verificar se x<0
     # verificar x par ou x impar
-    n = ceil((x.x - (ln2/2))/ln2) # deixar como constantes
-    r = (x.x - (n * ln2)) / 256
-    expr = 1 + r * (1 + r * (0.5 + r * (0.16666666666666666 + (0.041666666666666664 * r))))
-    expx = pow(2,n) * pow(expr,256) # soma do expoente com n
-    return expx
+    k1 = floor(x.x)
+    k2 = x.x - floor(x.x)
+    
+    if k1%2:
+        res = Dec2IEEE(pow(exp(k1/2), 2))
+    else:
+        res = Dec2IEEE(exp(1)*(pow(exp((k1-1)/2), 2)))
+    if k1 < 0:
+        res = Dec2IEEE(1/res.x)
+
+    n = ceil((k2 - (ln2/2))/ln2) # deixar como constantes
+    
+    r = (k2 - (n * ln2)) / 256
+    expr = Dec2IEEE(1 + r * (1 + r * (0.5 + r * (0.16666666666666666 + (0.041666666666666664 * r)))))
+    
+    expx = Dec2IEEE(pow(2,n) * pow(expr.x,256)) # soma do expoente com n
+
+    #ERRO NAO PODE SE PROPAGARRRRRRRRRRRRRRRRRRRRRRRRR
+    #print(expx.Fbits.e)
+    #print(res.Fbits.e)
+
+    res = Dec2IEEE(expx.x * res.x)
+    
+    return res.x
 
 def balley():
     argumentos = []
@@ -31,7 +85,7 @@ def balley():
         corretos.append(correto)
         erros.append(abs(correto - aproximado))
         aproximados.append(aproximado)
-    print(f"{'Argumento':<10}{'Valor Correto':<15}{'Erro':<10}{'Valor Aproximado':<20}")
+    print(f"{'Argumento':<10}{'Valor Correto':<25}{'Erro':<20}{'Valor Aproximado':<20}")
     print("-" * 55)
     for i in range(len(argumentos)):
         print(f"{argumentos[i]:<10}{corretos[i]:<15.8f}{erros[i]:<20.16f}{aproximados[i]:<30.8f}")
@@ -44,4 +98,6 @@ def balley():
     plt.show()
 
 if __name__ == "__main__":
-    balley()
+    #balley()
+    print(exponencial(Dec2IEEE(3.6)))
+    print(exp(3.6))
--- lut.py
+++ lut.py
@@ -3,15 +3,19 @@ from math import log
 
 def cria_lut() -> [[]]:
     lut = []
-    n = -1
+    n = 1
     while True:
-        lut.append([((2**n + 1)), log(2**n)])
-        n -= 1
-        if ((2**n + 1) == 1):
+        lut.append([n, ((2**(-n) + 1)), log((2**(-n))+1)])
+        n += 1
+        if ((2**(-n) + 1) == 1):
             break
     print(lut)
     return lut
 
+def algoritmo(x,lut):
+    j = 1
+    xj = x
+    y1 = 1
 
 if __name__ == "__main__":
     lut = cria_lut()
