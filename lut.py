72859bfe1df32d2bf65a60390643fb214ebb6e6f
BadenP <ptowersp@hotmail.com>
2024-01-28 18:24:56 -0300

    algoritmo da lut pronto

--- lut.py
+++ lut.py
@@ -1,5 +1,6 @@
 from IEEE754 import Dec2IEEE
-from math import log
+from math import log, exp
+import matplotlib.pyplot as plt
 
 def cria_lut() -> [[]]:
     lut = []
@@ -9,14 +10,46 @@ def cria_lut() -> [[]]:
         n += 1
         if ((2**(-n) + 1) == 1):
             break
-    print(lut)
     return lut
 
-def algoritmo(x,lut):
-    j = 1
+def algoritmo(x, lut):
+    j = 0
     xj = x
-    y1 = 1
+    yj = 1
+    
+    while xj != 0 and j < len(lut):
+        # Passo 3: Pesquisar na LUT o maior valor k tal que xj - k >= 0
+        linha = lut[j]
+        if xj - linha[2] >= 0:
+            max_k = linha[2]
+            xj = xj - max_k # Fazer xj+1 = xj - max(kLUT) para k <= xj
+            yj = yj * linha[1] # Multiplicar o valor yj pelo valor e^k correspondente
+
+        j += 1
+
+    # O resultado aproximado é armazenado em y1
+    return yj
+
+valores_aproximados = []
+valores_exatos = []
+erros = []
+
+def calculoLUT():
+    lut = cria_lut()
+    for i in range(0,100,3):
+        i /= 100
+        x = i
+        resultado_aproximado = algoritmo(x, lut)
+        valores_aproximados.append(resultado_aproximado)
+        valores_exatos.append(exp(x))
+        erros.append(abs(exp(x)-resultado_aproximado))
+
 
 if __name__ == "__main__":
-    lut = cria_lut()
-    x = Dec2IEEE(3.6)
+    calculoLUT()
+    print(valores_aproximados)
+    print(valores_exatos)
+    print(erros)
+    vetLegenda = [i/100 for i in range(0, 100,3)]
+    plt.plot(vetLegenda, erros) 
+    plt.show()
